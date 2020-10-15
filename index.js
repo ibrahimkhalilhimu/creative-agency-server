d48abf19739ed1d84881fff7701e68a14afebe98
ibrahimkhalilhimu <ibrahimkhalilhemu1@gmail.com>
2020-10-15 12:50:38 +0600

    addService solve

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const fs = require('fs-extra')
+const fileUpload = require('express-fileUpload') 
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
@@ -9,6 +11,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const app = express()
 app.use(bodyParser.json());
 app.use(cors())
+app.use(express.static('services'))
+app.use(fileUpload())
 const port = 5000
 
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true  });
@@ -17,14 +21,41 @@ client.connect(err => {
   const reviewCollection = client.db("CreativeAgency").collection("review");
   
   app.post('/addServices',(req,res)=>{
-    const services = req.body;
-    console.log(services);
-    servicesCollection.insertOne(services)
-    .then(result=>{
-      console.log(result.insertedCount);
-      res.send(result.insertedCount>0)
+    const file = req.files.file;
+    const title = req.body.title;
+    const description = req.body.description;
+    const filePath = `${__dirname}/services/${file.name}`
+    file.mv(filePath,err=>{
+      if (err) {
+        console.log(err);
+      res.status(500).send({msg:'failed to upload'})
+      }
+      const newImg = fs.readFileSync(filePath)
+      const encImg = newImg.toString('base64')
+
+      var image = {
+        contentType: file.mimetype,
+        size: file.size,
+        img: Buffer.from(encImg, 'base64')
+    };
+
+      servicesCollection.insertOne({title,description,image})
+      .then(result=>{
+        fs.remove(filePath,error=>{
+          if(error){
+            console.log(error)
+            res.status(500).send({msg:'failed to upload'})
+          }
+          res.send(result.insertedCount>0)
+        })
+        console.log(result.insertedCount);
+       
+      })
     })
-  })
+      // return res.send({name:file.name,path:`/${file.name}`})
+    })
+  
+ 
 
 
   app.get('/service', (req, res) => {
